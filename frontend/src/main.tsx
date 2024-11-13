46c15464d428307b4fde750ae656ec88ae048491
Aziiimm <azimrahat108@gmail.com>
2024-11-12 22:40:29 -0500

    updated styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,10 +4,17 @@ import "./index.css";
 import App from "./App.tsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+
+if (!domain || !clientId) {
+  console.error("Auth0 domain and clientId required.");
+}
+
 createRoot(document.getElementById("root")!).render(
   <Auth0Provider
-    domain={import.meta.env.VITE_AUTH0_DOMAIN}
-    clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+    domain={domain}
+    clientId={clientId}
     authorizationParams={{
       redirect_uri: window.location.origin,
     }}
