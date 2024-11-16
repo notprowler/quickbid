55ef1ac4e4387263243bb110063319c9cbfc7c7b
Aziiimm <azimrahat108@gmail.com>
2024-11-16 18:46:45 -0500

    added navbar, updated styling & file structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
+import NoNavbarLayout from "./layouts/NoNavbarLayout.tsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+
+        <Route element={<NoNavbarLayout />}>
+          <Route path="/login" element={<LoginPage />} />
+        </Route>
       </Routes>
     </Router>
   );
