463bf99b75697194bb1f94553d1c1bdba7086bfe
Arihant Tiwari <arihanttiwari2005@gmail.com>
2024-11-28 00:55:23 -0500

    Stripe Endpoint for transactions

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,19 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv";
+import cors from "cors";
+import Stripe from "stripe";
 import listingsRoutes from "@/routes/listings";
 import usersRoutes from "@/routes/users";
 import bidRouter from "@/routes/bids";
 
 import authRouter from "@/routes/auth";
 
+// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+
 const app: Application = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
@@ -25,3 +30,23 @@ app.use("/api/users", usersRoutes);
 app.use("/api/bids", bidRouter);
 
 app.use("/auth", authRouter);
+
+// // Stripe API for adding funds to account
+// app.post("/create-payment-intent", async (req, res) => {
+//   const { amount } = req.body; // uses amount in cents
+
+//   try {
+//     const paymentIntent = await stripe.paymentIntents.create({
+//       amount,
+//       currency: "usd",
+//       payment_method_types: ["card"],
+//     });
+
+//     res.status(200).json({
+//       clientSecret: paymentIntent.client_secret, // Send back the client secret
+//     });
+//   } catch (error: any) {
+//     console.error(error);
+//     res.status(500).json({ error: error.message });
+//   }
+// });
