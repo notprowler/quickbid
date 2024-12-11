5af685a82335335eb1d1356879d0732a0a7793f9
Arihant Tiwari <arihanttiwari2005@gmail.com>
2024-12-11 02:14:05 -0500

    Feat: Place a new bid

--- server/src/controllers/bids.Controller.ts
+++ server/src/controllers/bids.Controller.ts
@@ -103,6 +103,7 @@ const bidAccepted: RequestHandler = async (req: Request, res: Response) => {
     }
 }
 
+//1. if bid rejected then user money goes back to his wallet.
 const bidRejected: RequestHandler = async (req: Request, res: Response) => {
     const { itemID } = req.params;
     const {bidderID, bidValue} = req.body;
@@ -132,5 +133,101 @@ const bidRejected: RequestHandler = async (req: Request, res: Response) => {
     }
 }
 
+// PRE: check if user has enough money he put for bid.
+// PRE: Give back the old Bidders money back to his account
+// 1. deduct money from users account. 
+// 2. Update the New Highest bidder and bid amount
+// 3. If the deadline is hit then the listings becomes deactivated and owners gets to decide wether to sell or not
+// 4. 
+const placeBid: RequestHandler = async (req: Request, res: Response) => {
+    const { itemID } = req.params;
+    const { bidValue} = req.body;
 
-export default { newBid, retrieveAllBids, bidAccepted, bidRejected }
+    // TODO come back and change this value
+    const bidderID = req.user?.user_id || 104;
+   
+    if (!itemID || !bidderID || !bidValue) {
+        res.status(400).json({ error: 'Item ID, Bidder ID, Bid Value, and Bid Deadline are required' });
+        return;
+    }
+
+    try {
+// ------------------------------------------------------------------------------------------------
+        //PRE:
+        // Check if the user has sufficient funds for bid he put
+        const { data: userData, error: userError } = await supabase
+            .from('users')
+            .select('balance')
+            .eq('user_id', bidderID)
+            .single();
+
+        if (userError) throw userError;
+
+        if (userData.balance < bidValue) {
+            res.status(400).json({ error: 'Insufficient funds' });
+            return;
+        }
+// ------------------------------------------------------------------------------------------------
+        //PRE:
+        // Give back users money to his account.
+        const { data: previousBidData, error: previousBidError } = await supabase
+            .from('bids')
+            .select('bidder_id, bid_amount')
+            .eq('item_id', itemID)
+            .eq('bid_status', 'pending')
+            .single();
+
+        if (previousBidError) throw previousBidError;
+
+        if (previousBidData) {
+            const { bidder_id: oldBidder, bid_amount: oldAmount } = previousBidData;
+
+            const { data: refundData, error: refundError } = await supabase
+            .from('users')
+            .select('balance')
+            .eq('user_id', oldBidder)
+            .single();
+
+            if (refundError) throw refundError;
+
+            const newBalance = refundData.balance + oldAmount;
+
+            const { error: updateError } = await supabase
+            .from('users')
+            .update({ balance: newBalance })
+            .eq('user_id', oldBidder);
+
+            if (updateError) throw updateError;
+
+            if (refundError) throw refundError;
+        }
+// ------------------------------------------------------------------------------------------------
+        //1. Deduct money from users wallet
+        const { error: walletError } = await supabase
+            .from('users')
+            .update({ balance: userData.balance - bidValue })
+            .eq('user_id', bidderID);
+
+        if (walletError) throw walletError;
+// ------------------------------------------------------------------------------------------------
+
+        //2. update bidding table for new highest bidder and bid amount. 
+        const { data: bidData, error: bidError } = await supabase
+            .from('bids')
+            .update({ bid_amount: bidValue, bid_status: 'pending', bidder_id: bidderID })
+            .eq('item_id', itemID)
+            .select();
+
+        if (bidError) throw bidError;
+
+        res.status(200).json(bidData);
+    } catch (e) {
+        if (e instanceof Error) {
+            res.status(500).json({ error: `${e.message}` });
+        } else if (typeof e === 'object' && e !== null && 'message' in e) {
+            res.status(500).json({ error: `${e.message}` });
+        }
+    }
+}
+
+export default { newBid, retrieveAllBids, bidAccepted, bidRejected, placeBid }
--- server/src/routes/bids.ts
+++ server/src/routes/bids.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import bidsController from '@/controllers/bids.Controller';
+import { validateAccessToken } from '@/util/JWT';
 const router = express.Router();
 
 router.post('/:itemID', bidsController.newBid);
 router.get('/:itemID', bidsController.retrieveAllBids);
 router.put('/accept/:itemID', bidsController.bidAccepted);
 router.put('/reject/:itemID', bidsController.bidRejected);
-
+router.post('/place', validateAccessToken,bidsController.placeBid);
 export default router
