54f57b74117182eacc88784872ea98c1b20f2274
Arihant Tiwari <arihanttiwari2005@gmail.com>
2024-12-12 01:12:32 -0500

    feat: added categories create-listing

--- server/src/controllers/bids.Controller.ts
+++ server/src/controllers/bids.Controller.ts
@@ -68,30 +68,13 @@ const bidAccepted: RequestHandler = async (req: Request, res: Response) => {
       return;
     }
 
-    try {
-        const { data, error } = await supabase
-            .from('bids')
-            .update({ bid_status: 'accepted' })
-            .eq('item_id', itemID)
-            .eq('bidder_id', bidderID)
-            .eq('bid_amount', bidValue)
-            .select();
-
-        if (error) throw error;
-
-        if (!data || !data[0]) {
-            res.status(500).json({ error: 'Error updating row' });
-            return;
-        }
-
-        rejectAllBids(itemID, data[0].bid_id)
-        res.status(200).json(data);
-    } catch (e) {
-        if (e instanceof Error) {
-            res.status(500).json({ error: `${e.message}` });
-        } else if (typeof e === 'object' && e !== null && 'message' in e) {
-            res.status(500).json({ error: `${e.message}` });
-        }
+    await rejectAllBids(itemID, data[0].bid_id);
+    res.status(200).json(data);
+  } catch (e) {
+    if (e instanceof Error) {
+      res.status(500).json({ error: `${e.message}` });
+    } else if (typeof e === "object" && e !== null && "message" in e) {
+      res.status(500).json({ error: `${e.message}` });
     }
   }
 };
