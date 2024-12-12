4d7b0fcb52bdcff245f4bbc7c081898cf08d8c08
Arihant Tiwari <arihanttiwari2005@gmail.com>
2024-12-12 03:40:10 -0500

    more final changes

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -25,63 +25,24 @@ const router = express.Router();
 
 router.get("/profile", validateAccessToken, SuspensionPolicy, getUserProfile);
 router.get("/:id", getUser);
-router.delete("/:id", deleteUser);
-router.put("/:id", updateUser);
+router.get("/application/pending", validateAccessToken, SuspensionPolicy,getPendingUsers);
+router.get("/application/suspended", validateAccessToken, getSuspendedAccounts);
+router.get("/application/complaints",validateAccessToken,getPendingComplaints);
 
-router.put("/status/:id", updateUserStatus);
+router.put("/status/:id", validateAccessToken,updateUserStatus);
 
 /* Pending user form and SUPER USER responses*/
-router.get(
-  "/application/pending",
-  validateAccessToken,
-  SuspensionPolicy,
-  getPendingUsers
-);
 router.post("/approve/:id", validateAccessToken, approvePendingUser);
 router.post("/reject/:id", validateAccessToken, rejectPendingUser);
-router.post(
-  "/termination/:id",
-  validateAccessToken,
-  SuspensionPolicy,
-  AccountTerminationRequest
-);
+router.post("/termination/:id",validateAccessToken,SuspensionPolicy,AccountTerminationRequest);
+router.post("/profile-complaint/:id",validateAccessToken,SuspensionPolicy,ProfileUserComplaint);
+router.post("/cart-complaint/:id",validateAccessToken,SuspensionPolicy,CartUserComplaint);
 
-/* Complaints and Rating*/
-router.put(
-  "/rating/:id",
-  validateAccessToken,
-  SuspensionPolicy,
-  updateUserRating
-);
-router.post(
-  "/profile-complaint/:id",
-  validateAccessToken,
-  SuspensionPolicy,
-  ProfileUserComplaint
-);
-router.post(
-  "/cart-complaint/:id",
-  validateAccessToken,
-  SuspensionPolicy,
-  CartUserComplaint
-);
-router.put("/rating/:id", validateAccessToken, SuspensionPolicy, updateUser);
+router.put("/rating/:id",validateAccessToken,SuspensionPolicy,updateUserRating);
+// router.put("/rating/:id", validateAccessToken, SuspensionPolicy, updateUser);
+router.put("/lift-suspension/:id",validateAccessToken,SuspensionPolicy,LiftUserSuspension);
 
-/* User's status */
-router.put("/status/:id", validateAccessToken, updateUserStatus);
-router.put(
-  "/lift-suspension/:id",
-  validateAccessToken,
-  SuspensionPolicy,
-  LiftUserSuspension
-);
-router.get("/application/suspended", validateAccessToken, getSuspendedAccounts);
-
-router.get(
-  "/application/complaints",
-  validateAccessToken,
-  getPendingComplaints
-);
+router.delete("/:id", deleteUser);
 
 
 export default router;
